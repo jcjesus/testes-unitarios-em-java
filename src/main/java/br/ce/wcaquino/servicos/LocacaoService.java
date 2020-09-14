cefbf76c2dc59e6174ede7b3d9116d6cfaa62ba7
Julio Cesar Soares de Jesus <jjesus@brq.com>
2020-09-14 17:42:42 -0300

    Secao2 - aula5.

--- src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
+++ src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
@@ -4,6 +4,9 @@ import static br.ce.wcaquino.utils.DataUtils.adicionarDias;
 
 import java.util.Date;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
 import br.ce.wcaquino.entidades.Usuario;
@@ -29,32 +32,5 @@ public class LocacaoService {
 		return locacao;
 	}
 
-	public static void main(String[] args) {
-		
-		//cenário: local onde as variáveis serão inicializadas
-		Usuario u = new Usuario("Jesus");
-		final double valorLocacao = 8.0d;
-		Filme f = new Filme("Um tira da pesada", 2, valorLocacao);
-		
-		LocacaoService s = new LocacaoService();
-		
-		//acao: onde vamos invocar o método de teste
-		Locacao r = s.alugarFilme(u, f);
-		
-		//validacao: onde coletar os resultados da ação com o cenário especificado
-		// data da locação, preço da locação e data de retorno
-		final Date dtLoc = r.getDataLocacao();
-		final Double precoLoc = r.getValor();
-		final Date dtDevolucao = r.getDataRetorno();
-		
-		boolean foiLocadoHoje = DataUtils.isMesmaData(dtLoc, new Date());
-		boolean precoLocalCorreto = precoLoc.equals(valorLocacao);
-		boolean dtDevolucaoAmanha = DataUtils.isMesmaData(dtDevolucao, DataUtils.obterDataComDiferencaDias(1));
-
-		System.out.println(foiLocadoHoje);
-		System.out.println(precoLocalCorreto);
-		System.out.println(dtDevolucaoAmanha);
-		
-		
-	}
+	
 }
--- src/test/java/br/ce/wcaquino/servicos/LocalcaoServiceTest.java
+++ src/test/java/br/ce/wcaquino/servicos/LocalcaoServiceTest.java
@@ -3,10 +3,47 @@
  */
 package br.ce.wcaquino.servicos;
 
+import java.util.Date;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import br.ce.wcaquino.entidades.Filme;
+import br.ce.wcaquino.entidades.Locacao;
+import br.ce.wcaquino.entidades.Usuario;
+import br.ce.wcaquino.utils.DataUtils;
+
 /**
  * @author jjesus
  *
  */
 public class LocalcaoServiceTest {
+	@Test
+	public void teste() {
+		
+		//cenário: local onde as variáveis serão inicializadas
+		Usuario u = new Usuario("Jesus");
+		Filme f = new Filme("Um tira da pesada", 2, 8.0);
+		
+		LocacaoService s = new LocacaoService();
+		
+		//acao: onde vamos invocar o método de teste
+		Locacao r = s.alugarFilme(u, f);
+		
+		//validacao: onde coletar os resultados da ação com o cenário especificado
+		// data da locação, preço da locação e data de retorno
+		final Date dtLoc = r.getDataLocacao();
+		final Double precoLoc = r.getValor();
+		final Date dtDevolucao = r.getDataRetorno();
+		
+		boolean foiLocadoHoje = DataUtils.isMesmaData(dtLoc, new Date());
+		boolean precoLocalCorreto = precoLoc.equals(8.0);
+		boolean dtDevolucaoAmanha = DataUtils.isMesmaData(dtDevolucao, DataUtils.obterDataComDiferencaDias(1));
 
+		Assert.assertTrue(foiLocadoHoje);
+		Assert.assertTrue(precoLocalCorreto);
+		Assert.assertTrue(dtDevolucaoAmanha);
+		
+		
+	}
 }
