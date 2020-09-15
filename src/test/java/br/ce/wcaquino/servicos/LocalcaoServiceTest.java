9f2288b817c43178914cfed72bdbfadd38042fd3
Julio Cesar Soares de Jesus <jjesus@brq.com>
2020-09-14 21:11:17 -0300

    Secao2 - aula 7 e 8.

--- src/test/java/br/ce/wcaquino/servicos/LocalcaoServiceTest.java
+++ src/test/java/br/ce/wcaquino/servicos/LocalcaoServiceTest.java
@@ -5,8 +5,11 @@ package br.ce.wcaquino.servicos;
 
 import java.util.Date;
 
+import org.hamcrest.CoreMatchers;
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ErrorCollector;
 
 import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
@@ -18,6 +21,10 @@ import br.ce.wcaquino.utils.DataUtils;
  *
  */
 public class LocalcaoServiceTest {
+	
+	@Rule
+	public ErrorCollector error = new ErrorCollector();
+	
 	@Test
 	public void teste() {
 		
@@ -40,10 +47,40 @@ public class LocalcaoServiceTest {
 		boolean precoLocalCorreto = precoLoc.equals(8.0);
 		boolean dtDevolucaoAmanha = DataUtils.isMesmaData(dtDevolucao, DataUtils.obterDataComDiferencaDias(1));
 
+		Assert.assertThat(r.getValor(), CoreMatchers.is(8.0));
+		Assert.assertThat(r.getValor(), CoreMatchers.is(CoreMatchers.equalTo(8.0)));
+		Assert.assertThat(r.getValor(), CoreMatchers.is(CoreMatchers.not(9.0)));
+		Assert.assertEquals(8.0, r.getValor(), 0.01);
 		Assert.assertTrue(foiLocadoHoje);
 		Assert.assertTrue(precoLocalCorreto);
 		Assert.assertTrue(dtDevolucaoAmanha);
 		
 		
 	}
+	
+	@Test
+	public void testeUsandoErroCollector() {
+		
+		//cenário: local onde as variáveis serão inicializadas
+		Usuario u = new Usuario("Julio");
+		Filme f = new Filme("Robocop", 2, 8.0);
+		
+		LocacaoService s = new LocacaoService();
+		
+		//acao: onde vamos invocar o método de teste
+		Locacao locacao = s.alugarFilme(u, f);
+		
+		//validacao: onde coletar os resultados da ação com o cenário especificado
+		// data da locação, preço da locação e data de retorno
+		
+//		error.checkThat(locacao.getValor(), CoreMatchers.is(7.0));
+		error.checkThat(locacao.getValor(), CoreMatchers.is(8.0));
+		error.checkThat(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()), CoreMatchers.is(true));
+//		error.checkThat(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)), CoreMatchers.is(false));
+		error.checkThat(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)), CoreMatchers.is(true));
+		
+		
+	}
+	
+	
 }
